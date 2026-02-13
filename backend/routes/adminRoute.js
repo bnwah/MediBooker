f8fa3e27a695f9368994bb411a82b7083793b414
Blessing Nwah <bnwah@vt.edu>
2026-02-13 16:58:00 -0500

    Creating admin login functionality and generating token based on email and password match.

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addDoctor } from '../controllers/adminController.js'
+import { addDoctor, loginAdmin } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 
 const adminRouter = express.Router()
@@ -7,5 +7,8 @@ const adminRouter = express.Router()
 // Whenever this endpoint is called, the form data has to send the image
 // The middleware will process the image and form data 
 adminRouter.post('/add-doctor', upload.single('image'), addDoctor)
+
+// This endpoint is for Admin login
+adminRouter.post('/login', loginAdmin)
 
 export default adminRouter
