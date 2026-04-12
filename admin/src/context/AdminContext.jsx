cce587192d637abe545686dbc53215616195de0f
Blessing Nwah <bnwah@vt.edu>
2026-04-11 23:35:53 -0400

    Login page for Admin: onSubmitHandler created.

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,11 +1,17 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
+
+    const [adminToken, setAToken] = useState('')
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
     const value = {
         // Define any state or functions you want to provide to the context
-        
+        adminToken, setAToken,
+        backendUrl,
     }
 
     return (
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,21 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { assets } from '../assets/assets'
+import { AdminContext } from '../context/AdminContext'
+import axios from 'axios'
 
 const Login = () => {
 
     const [state, setState] = useState('Admin')
 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const {setAToken, backendUrl} = useContext(AdminContext)
+
+    // This fn will perform when the form is submitted
+    const onSubmitHandler = async (event) => {
+        event.preventDefault() /* whenever form is submitted, it will not reload pg as there's no need to! */
+
+        try {
+
+            if (state === 'Admin') {
+
+                const { data } = await axios.post(backendUrl + '/api/admin/login', email, password)
+                if (data.success) {
+                    console.log(data.token)
+                }
+            }
+            
+        } catch (error) {
+            
+        }
+    }
+
   return (
-    <form className='min-h-[80vh] flex items-center'>
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
         <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
             <p className='text-2xl font-semibold m-auto'><span className='text-primary'> {state} </span> Login!!👏</p>
             <div className='w-full'>
                 <p>Email</p>
-                <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />                
+                <input onClick={(e) => setEmail(e.target.value)} value={email} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />                
             </div>
             <div className='w-full'>
                 <p>Password</p>
-                <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />                
+                <input onClick={(e) => setPassword(e.target.value)} value={password} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />                
             </div>
             <button className='bg-primary text-white w-full py-2 rounded-md text-base'>Login</button>
 
@@ -24,7 +50,7 @@ const Login = () => {
                 ? <p>Doctor Login? <span className='text-primary underline cursor-pointer' onClick={() => setState('Doctor')}>Click here</span></p>
                 : <p>Admin Login? <span className='text-primary underline cursor-pointer' onClick={() => setState('Admin')}>Click here</span></p>
             }
-            
+
         </div>
 
     </form>
