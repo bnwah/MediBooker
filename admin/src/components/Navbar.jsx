c11f7ba37839e35f7fbfdee5f2be511be9a850ac
Blessing Nwah <bnwah@vt.edu>
2026-05-07 21:07:39 -0400

    Logout functionality added.

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -4,15 +4,20 @@ import { AdminContext } from '../context/AdminContext'
 
 const Navbar = () => {
 
-    const {adminToken} = useContext(AdminContext)
+  const { adminToken, setAToken } = useContext(AdminContext)
+
+  const logout = () => {
+    adminToken && setAToken('')
+    adminToken && localStorage.removeItem('adminToken')
+  }
 
   return (
     <div className='flex justify-between items-center px-4 sm:px-10  py-3 border-b bg-white'>
-        <div className='flex items-center gap-2 text-xs'>
-            <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
-            <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'> {adminToken? 'Admin' : 'Doctor'} </p>
-        </div>
-        <button className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
+      <div className='flex items-center gap-2 text-xs'>
+        <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
+        <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'> {adminToken ? 'Admin' : 'Doctor'} </p>
+      </div>
+      <button onClick={logout} className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
     </div>
   )
 }
